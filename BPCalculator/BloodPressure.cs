3a3826922faa3b1e7096dd33cf1b555f97509e69
Niksa Jadric <X00205794@myTUDublin.ie>
2023-11-30 22:03:43 +0000

    Added unit and acceptance tests in the pipeline yaml

--- BPCalculator/BloodPressure.cs
+++ BPCalculator/BloodPressure.cs
@@ -8,7 +8,7 @@ namespace BPCalculator
     // BP categories
     public enum BPCategory
     {
-        [Display(Name="Lowwwwww Blood Pressure")] Low,
+        [Display(Name="Low Blood Pressure")] Low,
         [Display(Name="Ideal Blood Pressure")]  Ideal,
         [Display(Name="Pre-High Blood Pressure")] PreHigh,
         [Display(Name="High Blood Pressure")]  High,
