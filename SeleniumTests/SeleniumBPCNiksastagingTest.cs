f65fb74912b40887b1b9141f988658a233c0e9f9
NiksaJadric <X00205794@myTUDublin.ie>
2023-12-06 21:21:15 +0000

    Fixing Selenium tests 7

--- SeleniumTests/SeleniumBPCNiksastagingTest.cs
+++ SeleniumTests/SeleniumBPCNiksastagingTest.cs
@@ -9,7 +9,7 @@ using OpenQA.Selenium.Chrome;
 //using OpenQA.Selenium.Firefox;
 //using OpenQA.Selenium.Remote;
 //using OpenQA.Selenium.Support.UI;
-//using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Interactions;
 using NUnit.Framework;
 
 namespace SeleniumTests
