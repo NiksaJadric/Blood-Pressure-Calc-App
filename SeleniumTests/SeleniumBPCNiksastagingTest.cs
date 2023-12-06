2ba30369b2c62f63008f2794fa8fb8ca40eac199
NiksaJadric <X00205794@myTUDublin.ie>
2023-12-06 19:05:19 +0000

    Fixing Selenium tests 1

--- SeleniumTests/SeleniumBPCNiksastagingTest.cs
+++ SeleniumTests/SeleniumBPCNiksastagingTest.cs
@@ -4,12 +4,12 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Remote;
-using OpenQA.Selenium.Support.UI;
-using OpenQA.Selenium.Interactions;
+//using OpenQA.Selenium;
+//using OpenQA.Selenium.Chrome;
+//using OpenQA.Selenium.Firefox;
+//using OpenQA.Selenium.Remote;
+//using OpenQA.Selenium.Support.UI;
+//using OpenQA.Selenium.Interactions;
 using NUnit.Framework;
 
 namespace SeleniumTests
