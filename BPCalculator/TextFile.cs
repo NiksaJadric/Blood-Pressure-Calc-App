a095e489971c6fad115a26fcadbb1bb38fcd9685
NiksaJadric <X00205794@myTUDublin.ie>
2023-12-17 16:38:24 +0000

    cleanup 2 - push again

--- BPCalculator/TextFile.cs
+++ BPCalculator/TextFile.cs
@@ -1,3 +1,5 @@
 ﻿// this is dummy file that will hopefully invoke medn bolt scan
 
 // added this line to see will scan still run and will Issues remain if I "git push" again - it did not run.
+
+// running hte pipeline again so that I can set staging slot to be the same as prod
